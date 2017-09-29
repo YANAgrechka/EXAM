694bf2c00f870a44ebd35e63617a0ee07421a96e
Coco-S <coco-s@list.ru>
2017-09-29 17:39:47 +0300

    ready

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -6,14 +6,54 @@ using System.Threading.Tasks;
 
 namespace ConsoleApplication1
 {
+    interface ICard
+    {
+        int Sum { get; set; }
+        int add(int a);
+        int del(int a);
+            
+    }
+    abstract class Address
+    {
+         public string address;
+    }
+    class Person : Address, ICard
+    {
+        public int sum;
+        public int Sum
+        {
+            get
+            { return sum; }
+            set
+            { sum = value; }
+        }
+
+
+        public int add(int a)
+        {
+            sum += a;
+            return sum;
+        }
+
+        public int del(int a)
+        {
+            sum -= a;
+            return sum;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            // заданиепаар
-            // pffl
-            // asdsads
-            // ты не сдашь
+            Person person = new Person();
+            person.sum = 10;
+            person.del(5);
+            Console.WriteLine(person.sum);
+            person.add(7);
+            Console.WriteLine(person.sum);
+            person.address = "Sverdlova 13a";
+            Console.WriteLine(person.address);
+
         }
     }
 }
